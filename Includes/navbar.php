8278f8ab0fd1d66ba1019050b0807c4a78fae713
John Doe <johndoe@email.com>
2024-09-13 02:26:49 +0700

    first commit

---
+++ Includes/navbar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../Assets\Styles\styles.css">
+    <link rel="stylesheet" href="../Assets\Styles\fonts.css">
+</head>
+
+<body>
+    <div class="container">
+        <nav>
+            <div class="logo_con">
+                <img width="60px" height="60px" src="../Assets\Images\Logos\logo_F1.png" alt="">
+            </div>
+            <ul class="menu_center">
+                <li><a href="">Home</a></li>
+                <li><a href="">Activity</a></li>
+            </ul>
+            <ul class="menu_icon">
+                <li><a href=""><i class="fa-solid  fa-right-from-bracket"></i></a></li>
+                <li><a href=""><i class="fa-solid fa-gear"></i></a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+
+</html>
